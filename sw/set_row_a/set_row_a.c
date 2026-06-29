b3a51c075f989ec7e4680f5b00c4995687936873
Marcel Martinz <ge87dix@linux21.clients.eikon.tum.de>
2026-06-29 18:35:17 +0200

    Adjusted Adress to talk to our Module

--- sw/set_row_a/set_row_a.c
+++ sw/set_row_a/set_row_a.c
@@ -8,7 +8,7 @@ int main(){
   ss_init(1);
   volatile uint32_t temp_0 = 0x08;
   // this is basic read operation
-  temp_0 = *( volatile uint32_t* ) 0x01050020;
+  temp_0 = *( volatile uint32_t* ) 0x01051020;
   if (temp_0 == 0x08){
     errors++;
   }
@@ -16,7 +16,7 @@ int main(){
   // this is basic write operation
   *( volatile uint32_t* ) 0x01050020 = 0x08;
 
-  temp_0 = *( volatile uint32_t* )0x01050020;
+  temp_0 = *( volatile uint32_t* )0x01051020;
 
   if(temp_0 != 0x08){
     errors++;
